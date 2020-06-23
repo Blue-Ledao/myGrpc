23714dcd9d8c94360d40ffba07a0a2ad741f754e
田津津 <jinjin.tian@baishancloud.com>
2020-06-23 12:13:04 +0800

    实现在控制台显示登录相关的菜单界面

--- main/grpc_client.cc
+++ main/grpc_client.cc
@@ -21,6 +21,9 @@ using mygrpc::User;
 using mygrpc::Reply;
 using namespace std;
 
+const string USERNAME_OR_PASSWORD_ERROR = "用户名或者密码错误";
+const string ADD_ERROR = "ADD_ERROR";
+User currentUser;
 
 class LoginClient {
  public:
@@ -44,8 +47,57 @@ private:
 };
 
 int main(int argc, char** argv) {
-    
-  return 0;
+    while (true){
+          int opt = 0;
+          std::cout << "主菜单" << std::endl;
+          std::cout << "1 : " << "注册" << std::endl;
+          std::cout << "2 : " << "登录系统" << std::endl;
+          std::cout << "3 : " << "离开程序" << std::endl;
+          std::cin >> opt;
+          std::string username;
+          std::string pwd;
+          switch (opt){
+                  case 1:{
+                      std::cout << "请输入用户名 : "; std::cin >> username;
+                      std::cout << "请输入用密码 : "; std::cin >> pwd;
+                                          
+                      std::string reply = "";
+                      if(reply != ADD_ERROR){
+                          std::cout << "注册用户成功！\t"  <<  std::endl;
+                      }else{
+                          std::cout << "注册用户失败！\t"  <<  std::endl;
+                      }
+                      
+                      break;
+                  }
+                  case 2:{
+                      std::cout << "请输入用户名 : "; std::cin >> username;
+                      std::cout << "请输入用密码 : "; std::cin >> pwd;
+                      currentUser.set_name(username);
+                      
+                      std::string reply = "";
+                      
+                      std::cout << reply << std::endl;
+                      if(reply != USERNAME_OR_PASSWORD_ERROR){
+                          std::cout << "登录成功，输入'logout'退出当前登录，并返回主菜单...." << std::endl;
+                          while(true){
+                              std::string command;
+                              std::cin >> command;
+                              if(command == "logout"){
+                                  break;
+                              }else{
+                                  std::cout << "指令非法...." << std::endl;
+                              }
+                          }
+                      }
+                      break;
+                  }
+                  case 3: {
+                      return 0;
+                  }
+          }
+      }
+    return 0;
 }
 
 
